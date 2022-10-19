6435f070cbecec28a8fbeb44338307679d7a3a48
akimtspro <alexandre.kimtsaris@outlook.com>
2022-10-19 15:51:22 +0200

    exo plateau corrigé

--- src/mobile/exo/Plateau.java
+++ src/mobile/exo/Plateau.java
@@ -6,9 +6,56 @@ public class Plateau {
 
     public static void main(String[] args) {
 
-        // ton code
         Scanner sc = new Scanner( System.in );
-        char rslt = sc.nextLine().charAt(0);
+        char[] plateau = { '_', '_', '_', '_', '*' };
+        int position = 4;
+
+        char choix;
+        do {
+
+            for (int i = 0; i < plateau.length; i++) {
+                System.out.print( plateau[i] + " " );
+            }
+            System.out.println();
+
+            choix = sc.nextLine().charAt(0);
+
+            switch ( choix ){
+
+                case 'd':
+                    if( position == 4 ){
+                        plateau[position] = '_';
+                        position = 0;
+                        plateau[position] = '*';
+                    }
+                    else {
+                        plateau[position] = '_';
+                        plateau[++position] = '*';
+                    }
+                    break;
+
+                case 'g':
+                    if( position == 0 ){
+                        plateau[position] = '_';
+                        position = 4;
+                        plateau[position] = '*';
+                    }
+                    else {
+                        plateau[position] = '_';
+                        plateau[--position] = '*';
+                    }
+                    break;
+                case 'q':
+                    System.out.println("Au revoir");
+                    break;
+                default:
+                    System.out.println("Choix invalide");
+
+            }
+
+        } while( choix != 'q' );
+
+
 
 
     }
---
+++ src/mobile/exo/ToucheCoule.java
@@ -0,0 +1,15 @@
+package mobile.exo;
+
+import java.util.Random;
+
+public class ToucheCoule {
+
+    public static void main(String[] args) {
+
+        // Je veux un entier entre 0 et 9
+        Random rdm = new Random();
+        int rdmized = rdm.nextInt(10);
+
+    }
+
+}
