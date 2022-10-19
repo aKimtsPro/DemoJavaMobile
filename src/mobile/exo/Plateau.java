903a696a5c8749cecfb09e4bdc6e0f826c639901
akimtspro <alexandre.kimtsaris@outlook.com>
2022-10-19 14:26:11 +0200

    exo plateau

--- src/mobile/exo/Plateau.java
+++ src/mobile/exo/Plateau.java
@@ -1,10 +1,15 @@
 package mobile.exo;
 
+import java.util.Scanner;
+
 public class Plateau {
 
     public static void main(String[] args) {
 
         // ton code
+        Scanner sc = new Scanner( System.in );
+        char rslt = sc.nextLine().charAt(0);
+
 
     }
 
