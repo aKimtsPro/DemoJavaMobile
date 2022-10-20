793e1c8be57aabd6c3e9c8c7c62345647d34f988
akimtspro <alexandre.kimtsaris@outlook.com>
2022-10-20 09:40:52 +0200

    demo multi-dim

--- src/mobile/demo/DemoTableau.java
+++ src/mobile/demo/DemoTableau.java
@@ -18,8 +18,26 @@ public class DemoTableau {
         // Pour faire la même chose hors ce contexte
         tabEntier = new int[]{2,3};
 
+        // int[] <=> tableauDeInt
+        // tableauDeInt[]
+        int[][] tabDeTabDeInt = new int[2][2];
+        tabDeTabDeInt = new int[2][];
+        tabDeTabDeInt[0] = new int[2];
+        tabDeTabDeInt[1] = new int[1];
 
+        int[] unDesTab = tabDeTabDeInt[0];
+        unDesTab[1] = 5;
 
+        tabDeTabDeInt[0][1] = 5;
+
+        // int[][][]
+        int[][][] tabDeTabDeTabDeInt = new int[1][1][1];
+        // int[][]
+        tabDeTabDeTabDeInt[0] = new int[1][1];
+        // int[]
+        tabDeTabDeTabDeInt[0][0] = new int[1];
+        // int
+        tabDeTabDeTabDeInt[0][0][0] = 1;
 
     }
 
